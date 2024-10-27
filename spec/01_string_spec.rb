68e61768426b77597993944a0b2df1e645ab113a
t-drape <thebatreview1@gmail.com>
2024-10-27 11:37:04 -0700

    Finish Lesson 1

--- spec/01_string_spec.rb
+++ spec/01_string_spec.rb
@@ -39,18 +39,20 @@ end
 
 describe String do
   # Create a let variable that will pass the first test.
+  let(:favorite_food) { String.new('tacos') }
 
   # remove the 'x' before running this test
-  xit 'is equal to tacos' do
+  it 'is equal to tacos' do
     expect(favorite_food).to eq('tacos')
   end
 
   # remove the 'x' before running this test
   context 'when favorite food is updated' do
     # Change the favorite_food let variable.
+    let(:favorite_food) { String.new('burgers') }
 
-    xit 'updates the favorite food' do
-      # Write a test that will pass.
+    it 'updates the favorite food' do
+      expect(favorite_food).to eq('burgers')
     end
   end
 end
